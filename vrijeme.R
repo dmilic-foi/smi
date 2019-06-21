d82dacbcea3922e08784464a6b509e5582f1fb52
dmilic-foi <dmilic@foi.hr>
2019-06-21 17:29:23 +0200

    Implementirano prebrojavanje podataka po godišnjim dobima

--- vrijeme.R
+++ vrijeme.R
@@ -3,6 +3,15 @@ vrijeme1 <- read.delim("./weather1.txt", header=T, sep="\t")
 
 Sys.setlocale("LC_TIME", "Croatian")
 
+# Prebrojava neprazne podatke u svakom stupcu zadanog dataseta
+countData <- function(dataset) {
+  count_dataset <- c()
+  for (i in 1:ncol(dataset)) {
+    count_dataset[i] <- sum(dataset[i]!='' & !is.na(dataset[i]))
+  }
+  return(setNames(count_dataset, names(dataset)))
+}
+
 godisnjeDobaEnum <- function() {
   list(ZIMA=0, PROLJECE=1, LJETO=2, JESEN=3)
 }
@@ -16,12 +25,13 @@ getGodisnjeDoba <- function(datum) {
   # Convert year to 2000
   datum <- as.Date(strftime(datum, format="2000-%m-%d"))
   
-  if (datum >= proljece && datum < ljeto) return(godisnjeDobaEnum()$PROLJECE)
-  else if (datum >= ljeto && datum < jesen) return(godisnjeDobaEnum()$LJETO)
-  else if (datum >= jesen && datum < zima) return(godisnjeDobaEnum()$JESEN)
-  else if (datum >= zima || datum < proljece) return(godisnjeDobaEnum()$ZIMA)
+  if (datum >= proljece & datum < ljeto) return(godisnjeDobaEnum()$PROLJECE)
+  else if (datum >= ljeto & datum < jesen) return(godisnjeDobaEnum()$LJETO)
+  else if (datum >= jesen & datum < zima) return(godisnjeDobaEnum()$JESEN)
+  else if (datum >= zima | datum < proljece) return(godisnjeDobaEnum()$ZIMA)
 }
 
+# Izracunava godisnja doba za svaki datum u datasetu
 godisnjaDoba <- c()
 for (i in 1:nrow(vrijeme1)) {
   datum <- gsub("velj", "veljaca", vrijeme1[i,]$Date)
@@ -34,4 +44,18 @@ for (i in 1:nrow(vrijeme1)) {
   godisnjaDoba[i] <- godisnjeDoba
 }
 
+# Dodaje novu varijablu s godisnjim dobima 
 vrijeme1$god_doba <- godisnjaDoba
+
+# Razvrstava podskupove po godisnjim dobima
+vrijeme1_zima = subset(vrijeme1, god_doba==0)[names(vrijeme1) != "god_doba"]
+vrijeme1_proljece = subset(vrijeme1, god_doba==1)[names(vrijeme1) != "god_doba"]
+vrijeme1_ljeto = subset(vrijeme1, god_doba==2)[names(vrijeme1) != "god_doba"]
+vrijeme1_jesen = subset(vrijeme1, god_doba==3)[names(vrijeme1) != "god_doba"]
+
+# Prebrojavanje podataka za svako godisnje doba
+count_zima <- countData(vrijeme1_zima)
+count_proljece <- countData(vrijeme1_proljece)
+count_ljeto <- countData(vrijeme1_ljeto)
+count_jesen <- countData(vrijeme1_jesen)
+
